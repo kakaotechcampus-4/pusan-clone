cb791a9e1e38041dabdc9c628fe6a21adb1d81c5
tomchccom <dreamkms2014@gmail.com>
2026-07-15 11:16:50 +0900

    feat(noraml) : 저장 입력도 정규화

--- student_parts/week03_build_nanas_logbook.py
+++ student_parts/week03_build_nanas_logbook.py
@@ -244,7 +244,21 @@ def _save_input_from(value: SaveStructuredRequestInput | StructuredRequest | dic
     """저장 입력을 SaveStructuredRequestInput 하나로 모읍니다."""
 
     # TODO: dict/JSON/자연어/StructuredRequest 입력을 SaveStructuredRequestInput으로 검증하고 정규화하세요.
-    ...
+    if isinstance(value , SaveStructuredRequestInput):
+        return value
+    if isinstance(value, StructuredRequest):
+        return SaveStructuredRequestInput(**value.model_dump())
+    if isinstance(value, dict):
+        return SaveStructuredRequestInput(**value)
+    if isinstance(value, str):
+        # json 인 경우 
+        try:
+            parsed = json.loads(value)
+            return SaveStructuredRequestInput(**parsed)
+        except (json.JSONDecodeError, TypeError):
+            sr = extract_structured_request(value)                                               
+            return SaveStructuredRequestInput(**sr.model_dump())
+    
 
 
 def save_structured_request_payload(
@@ -255,6 +269,8 @@ def save_structured_request_payload(
     """검증된 structured request를 앱 DB에 저장합니다."""
 
     # TODO: 입력을 검증한 뒤 AppSQLiteStore.save_structured_request(...)로 저장하고 tool 결과를 반환하세요.
+    
+    
     ...
 
 
