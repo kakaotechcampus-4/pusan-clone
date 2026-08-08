fda7b8060fb192d1720305a268a60cc205952825
jaeseong <wotjd4636@gmail.com>
2026-08-09 02:10:05 +0900

    week6: find_common_available_slots에서 나 멤버 중복 제거

--- student_parts/week06_kanamate_decides_schedule.py
+++ student_parts/week06_kanamate_decides_schedule.py
@@ -400,7 +400,7 @@ def find_common_available_slots_dict(
         busy_rows = collected.get("rows", [])
 
     return find_common_available_slots_payload(
-        member_names=["나", *normalized_members],
+        member_names=["나", *[name for name in normalized_members if name != "나"]],
         date_from=normalized_date_from,
         date_to=normalized_date_to,
         busy_rows=busy_rows,
